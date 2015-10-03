443ddd9cfef19cc9395e723904e744363208c591
Humberd <sawik11@wp.pl>
2015-10-03 21:00:07 +0200

    7

---
+++ Makao/src/karty/Talia.java
@@ -0,0 +1,18 @@
+package karty;
+
+import java.util.Stack;
+
+public class Talia extends Stack<Karta>{
+
+	private static final long serialVersionUID = 1923695545987559264L;
+	
+	Karta[] talia;
+	
+	
+	
+	public void tasuj() {
+		
+	}
+	
+
+}

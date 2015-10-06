fa92b30a3c97f40598954ce45f4a189bf9b161bc
Humberd <sawik11@wp.pl>
2015-10-06 20:51:28 +0200

    16 abstract

--- Makao/src/glowny/Main.java
+++ Makao/src/glowny/Main.java
@@ -1,5 +1,8 @@
 package glowny;
 
+import java.io.Console;
+import java.io.PrintWriter;
+
 import exceptions.CardException;
 import gracze.Czlowiek;
 import gracze.Gracz;
@@ -20,13 +23,16 @@ public class Main {
 		Gra gra = new Gra();
 		Gracz gracz = new Czlowiek();
 		Gracz gracz2 = new Czlowiek();
-		System.out.println(gra.dodajGraczaDoGry(gracz));
-		System.out.println(gra.dodajGraczaDoGry(gracz2));
-		System.out.println(gra.usunGraczaZGry(gracz));
-		System.out.println(gra.usunGraczaZGry(gracz2));
-
+//		System.out.println(gra.dodajGraczaDoGry(gracz));
+//		System.out.println(gra.dodajGraczaDoGry(gracz2));
+//		System.out.println(gra.usunGraczaZGry(gracz));
+//		System.out.println(gra.usunGraczaZGry(gracz2));
+		System.out.println("przed");
+		Console konsola = System.console();
+		PrintWriter out = konsola.writer();
 		
-
+		out.println("printwriter");
+		System.out.println("po");
 	}
 
 }
--- Makao/src/gracze/Gracz.java
+++ Makao/src/gracze/Gracz.java
@@ -4,6 +4,19 @@ import karty.Karta;
 import rozgrywka.Gra;
 
 public abstract class Gracz {
+	/**
+	 * Instancja gry, w której przebywa gracz
+	 */
 	private Gra gra;
+	
+	/**
+	 * 
+	 */
 	private Karta[] reka;
+	private Karta[] wybraneKarty;
+	
+	/**
+	 * @return
+	 */
+	public abstract boolean zmienKolor(); 
 }
